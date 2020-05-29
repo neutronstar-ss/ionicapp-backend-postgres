82c842fca68b47093137156d3a4fd8d1a99c2c3a
neutronstar-ss <neutron.star.system.services@gmail.com>
2020-05-29 19:12:26 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/neutronstar/ionicapp/services/CategoriaService.java
+++ src/main/java/com/neutronstar/ionicapp/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.neutronstar.ionicapp.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.neutronstar.ionicapp.domain.Categoria;
 import com.neutronstar.ionicapp.repositories.CategoriaRepository;
+import com.neutronstar.ionicapp.services.exceptions.DataIntegrityException;
 import com.neutronstar.ionicapp.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -20,4 +22,24 @@ public class CategoriaService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 		}
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
+	}
 }
