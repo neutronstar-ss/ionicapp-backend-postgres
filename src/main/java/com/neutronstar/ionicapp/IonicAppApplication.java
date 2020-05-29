1a43b05300b9e452ca0af642504d1247b14790b6
neutronstar-ss <neutron.star.system.services@gmail.com>
2020-05-29 15:17:08 -0300

    third commit

--- src/main/java/com/neutronstar/ionicapp/IonicAppApplication.java
+++ src/main/java/com/neutronstar/ionicapp/IonicAppApplication.java
@@ -1,5 +1,6 @@
 package com.neutronstar.ionicapp;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.neutronstar.ionicapp.domain.Cidade;
 import com.neutronstar.ionicapp.domain.Cliente;
 import com.neutronstar.ionicapp.domain.Endereco;
 import com.neutronstar.ionicapp.domain.Estado;
+import com.neutronstar.ionicapp.domain.Pagamento;
+import com.neutronstar.ionicapp.domain.PagamentoComBoleto;
+import com.neutronstar.ionicapp.domain.PagamentoComCartao;
+import com.neutronstar.ionicapp.domain.Pedido;
 import com.neutronstar.ionicapp.domain.Produto;
+import com.neutronstar.ionicapp.domain.enums.EstadoPagamento;
 import com.neutronstar.ionicapp.domain.enums.TipoCliente;
 import com.neutronstar.ionicapp.repositories.CategoriaRepository;
 import com.neutronstar.ionicapp.repositories.CidadeRepository;
 import com.neutronstar.ionicapp.repositories.ClienteRepository;
 import com.neutronstar.ionicapp.repositories.EnderecoRepository;
 import com.neutronstar.ionicapp.repositories.EstadoRepository;
+import com.neutronstar.ionicapp.repositories.PagamentoRepository;
+import com.neutronstar.ionicapp.repositories.PedidoRepository;
 import com.neutronstar.ionicapp.repositories.ProdutoRepository;
 
 
@@ -38,6 +46,10 @@ public class IonicAppApplication implements CommandLineRunner {
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(IonicAppApplication.class, args);
@@ -87,6 +99,22 @@ public class IonicAppApplication implements CommandLineRunner {
 		
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+				
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 	
 	}
 }
