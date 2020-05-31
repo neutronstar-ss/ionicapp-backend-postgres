fb90c7caf674f5f3d465427e1130b73d3203ff94
neutronstar-ss <neutron.star.system.services@gmail.com>
2020-05-31 08:56:12 -0300

    Validação customizada: email não repetido na atualização de cliente

--- src/main/java/com/neutronstar/ionicapp/dto/ClienteDTO.java
+++ src/main/java/com/neutronstar/ionicapp/dto/ClienteDTO.java
@@ -8,8 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.neutronstar.ionicapp.domain.Cliente;
+import com.neutronstar.ionicapp.services.validation.ClienteUpdate;
 
-
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
