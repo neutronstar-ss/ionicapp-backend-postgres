e05c15a6d9c8fcfe271dd92849d5f38e0ccc91be
neutronstar-ss <neutron.star.system.services@gmail.com>
2020-06-01 18:06:53 -0300

    Configuração inicial do Spring Security

--- src/main/java/com/neutronstar/ionicapp/services/EmailService.java
+++ src/main/java/com/neutronstar/ionicapp/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.neutronstar.ionicapp.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.neutronstar.ionicapp.domain.Pedido;
@@ -9,5 +11,9 @@ public interface EmailService {
 	void senderOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 
 }
