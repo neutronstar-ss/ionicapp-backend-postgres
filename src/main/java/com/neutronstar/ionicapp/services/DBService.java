2086d1f6c2f9164a0b7d3e406850c9965d5471ad
neutronstar-ss <neutron.star.system.services@gmail.com>
2020-06-01 19:32:09 -0300

    Salvando perfis de usuário na base de dados

--- src/main/java/com/neutronstar/ionicapp/services/DBService.java
+++ src/main/java/com/neutronstar/ionicapp/services/DBService.java
@@ -20,6 +20,7 @@ import com.neutronstar.ionicapp.domain.PagamentoComCartao;
 import com.neutronstar.ionicapp.domain.Pedido;
 import com.neutronstar.ionicapp.domain.Produto;
 import com.neutronstar.ionicapp.domain.enums.EstadoPagamento;
+import com.neutronstar.ionicapp.domain.enums.Perfil;
 import com.neutronstar.ionicapp.domain.enums.TipoCliente;
 import com.neutronstar.ionicapp.repositories.CategoriaRepository;
 import com.neutronstar.ionicapp.repositories.CidadeRepository;
@@ -115,16 +116,21 @@ public class DBService {
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 
 		Cliente cli1 = new Cliente(null, "Maria Silva", "ricardo.agnello.neto@gmail.com", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123"));
-
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+		
+		Cliente cli2 = new Cliente(null, "Cristiane Fernandes", "motoristaricardoagnello@gmail.com", "56135527037", TipoCliente.PESSOAFISICA, pe.encode("123"));
+		cli2.addPerfil(Perfil.ADMIN);
+		cli2.getTelefones().addAll(Arrays.asList("27558997", "955241258"));
 
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		Endereco e3 = new Endereco(null, "Avenida Carioca", "447", "Casa 3", "Moema", "08877025", cli2, c2);
 
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		cli2.getEnderecos().addAll(Arrays.asList(e3));
 
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		clienteRepository.saveAll(Arrays.asList(cli1, cli2));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2, e3));
 
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 
