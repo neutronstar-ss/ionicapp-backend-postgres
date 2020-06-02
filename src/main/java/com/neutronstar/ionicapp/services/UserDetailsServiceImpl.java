025d5f282f07eee13293af7b504516cc82ff1569
neutronstar-ss <neutron.star.system.services@gmail.com>
2020-06-01 21:19:06 -0300

    Implementando autenticação e geração do token JWT

---
+++ src/main/java/com/neutronstar/ionicapp/services/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.neutronstar.ionicapp.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.neutronstar.ionicapp.domain.Cliente;
+import com.neutronstar.ionicapp.repositories.ClienteRepository;
+import com.neutronstar.ionicapp.security.UserSS;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Cliente cli = repo.findByEmail(email);
+		if (cli == null) {
+			throw new UsernameNotFoundException(email);
+		}
+		return new UserSS(cli.getId(), cli.getEmail(), cli.getSenha(), cli.getPerfis());
+	}
+}
